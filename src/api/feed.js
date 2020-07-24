3fbfec3f033be66cc05708f69042743c28b87eb1
Diogo Bastos <diogo.bastos@hmhco.com>
2020-07-23 21:46:22 -0400

    Changes to run it in iOS and Android

--- src/api/feed.js
+++ src/api/feed.js
@@ -1,5 +1,11 @@
+import { Platform } from "react-native";
+
 const obterInformacoes = async (setInformacoes) => {
-    const infoHttp = await fetch("http://localhost:3030/feed");
+    //const infoHttp = await fetch("http://104.163.188.217:3030/feed");
+    const url = Platform.OS === 'android' ? '10.0.2.2' : 'localhost';
+
+    const infoHttp = await fetch(`http://${url}:3030/feed`);
+
     const infoJson = await infoHttp.json();
 
     setInformacoes(infoJson);
